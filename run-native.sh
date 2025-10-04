d2ba9cea6d3aa326e6036e2146a03cfc327b65a0
johnhegs <john@hegs.net>
2025-10-04 15:28:43 +0100

    Fix colour code escape sequences in native setup script

--- run-native.sh
+++ run-native.sh
@@ -6,12 +6,12 @@
 set -e  # Exit on error (but we'll handle errors gracefully with traps)
 
 # Color codes for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+CYAN=$'\033[0;36m'
+NC=$'\033[0m' # No Color
 
 # Helper functions
 print_header() {
